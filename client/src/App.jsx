762f7fb8a5113771b1f9bc22ef6623f07b7befe4
x20surya <suryanshuc659@gmail.com>
2024-08-10 13:03:35 +0530

    feat : "fix route"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={Login}/>
-        <Route path="/signup" element={Signup}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </>
   );
