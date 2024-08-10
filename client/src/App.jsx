fdeb2ca219e9a1a0bc7a5ae768c3b8ebb3258399
x20surya <suryanshuc659@gmail.com>
2024-08-10 12:46:50 +0530

    feat: "set up client"

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-6xl'>hello</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
